cbc12f5933d382d3d0b04ed8fd8e63244815650d
Roger Laura <roylauracaceres@gmail.com>
2022-10-18 23:49:01 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleCola/Cola.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCola
+{
+    internal class Cola
+    {
+        private int primero, ultimo,maxcola;
+        private int[] contenedorcola;
+        public Cola(int maxcola)
+        {
+            this.primero = 0;
+            this.ultimo = -1;
+            this.maxcola = maxcola;
+            this.contenedorcola = new int[maxcola]; 
+        }
+        public bool colavacia()
+        {
+            if (this.ultimo < this.primero)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool colallena()
+        {
+            if (this.ultimo == maxcola - 1&&this.primero==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void insertarItem(int dato)
+        {
+            if (this.colallena())
+            {
+                Console.WriteLine("\t LA COLA ESTA LLENA");
+            }
+            else
+            {
+                this.ultimo++;
+                this.contenedorcola[ultimo] = dato;
+                Console.WriteLine("\t SE INSERTO CON EXITO A LA COLA");
+            }
+        }
+        public void eliminarItem()
+        {
+            if (colavacia())
+            {
+                Console.WriteLine("\t LA COLA ESTA VACIA");
+            }
+            else
+            {
+                Console.WriteLine("\t SE ELEIMINO CON EXITO EL ELEMENTO "+this.contenedorcola[primero]);
+                for(int i=0; i<this.ultimo; i++)
+                {
+                    this.contenedorcola[i] = this.contenedorcola[i + 1];
+                }
+                this.ultimo--;
+            }
+        }
+        public void ListarItems()
+        {
+            if (this.colavacia())
+            {
+                Console.WriteLine("\tLA COLA ESTA VACIA");
+            }
+            else
+            {
+                for (int i = this.primero; i <= this.ultimo; i++)
+                {
+                    Console.WriteLine("\t [" + i + "] => " + this.contenedorcola[i]);
+                }
+            }
+        }
+
+    }
+}
---
+++ ConsoleCola/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dato, opcion;
+            Cola cola=new Cola(4);
+            do
+            {
+                Console.WriteLine("\t==================== [ MENU COLA ] =============================");
+                Console.WriteLine("\t\t1.- INSERTAR ELEMENTO ");
+                Console.WriteLine("\t\t2.- ELIMINAR ELEMENTO ");
+                Console.WriteLine("\t\t3.- LISTAR");
+                Console.WriteLine("\t\t4.- SALIR");
+                Console.Write("\tELIJA UNA OPCION: ");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("\tDigite un valor: ");
+                        dato = int.Parse(Console.ReadLine());
+                        cola.insertarItem(dato);
+                        break;
+                    case 2:
+                        cola.eliminarItem();
+                        break;
+                    case 3:
+                        cola.ListarItems();
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("\tOpcion no valida");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+            } while (opcion != 4);
+
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
